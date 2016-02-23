e023c1580b3cd043637d966b58e488c8fe977ecb
[David Anglin] <[DAAnglin6389@gmail.com]>
2016-02-23 10:08:59 -0800

    initial commit

---
+++ HappySad/ExpressionView.swift
@@ -0,0 +1,21 @@
+//
+//  ExpressionView.swift
+//  HappySad
+//
+//  Created by David Anglin on 2/23/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+import UIKit
+
+class ExpressionView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HappySadViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappySadViewController.swift
+//  HappySad
+//
+//  Created by David Anglin on 2/23/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+import UIKit
+
+class HappySadViewController: UIViewController {
+
+   
+
+}
